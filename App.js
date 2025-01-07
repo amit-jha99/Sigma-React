30fc4a24ad234e4b09f630345b624f01c36e3ac0
amit-jha99 <amitjha121299@gmail.com>
2025-01-08 01:10:59 +0530

    Refactor App.js to create a nested structure for the heading element and log the parent component

--- App.js
+++ App.js
@@ -1,3 +1,13 @@
-const heading = React.createElement('h1', {id:"heading"}, 'Hello, React!');   
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(heading)
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement("h1", { id: "heading" },"Hello I am a heading inside nested elements")
+  )
+);
+// const heading = React.createElement("h1", { id: "heading" }, "Hello, React!");
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
