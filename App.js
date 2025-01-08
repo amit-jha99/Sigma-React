e375d61a3260e5cbf4d5a66b78b746ec92cc5eea
amit-jha99 <amitjha121299@gmail.com>
2025-01-09 00:42:00 +0530

    Add nested heading elements in App.js for improved structure

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 const parent = React.createElement(
   "div",
   { id: "parent" },
-  React.createElement(
-    "div",
-    { id: "child" },
-    React.createElement("h1", { id: "heading" },"Hello I am a heading inside nested elements")
-  )
+  React.createElement("div", { id: "child" }, [
+    React.createElement(
+      "h1",
+      { id: "heading" },
+      "Hello I am a heading inside nested elements"
+    ),
+    React.createElement(
+      "h2",
+      { id: "heading" },
+      "Hello I am a heading h2 inside nested elements"
+    ),
+  ])
 );
 // const heading = React.createElement("h1", { id: "heading" }, "Hello, React!");
 console.log(parent);
