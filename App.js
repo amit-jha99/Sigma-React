f0e46add05a26741b46fd4b9be3303a0501b7dd4
amit-jha99 <amitjha121299@gmail.com>
2025-01-20 00:22:34 +0530

    Refactor App.js to use JSX and introduce HeadingComponent for better structure

--- App.js
+++ App.js
@@ -3,6 +3,23 @@ import ReactDOM from "react-dom/client";
 
 // React.createElement =>Object=>HTML Element ( when we render it on the DOM)
 
-const heading = React.createElement("h1", {id:"heading"}, "Tough times create strong men");
+//JSX
+const heading = (
+  <h1 className="heading" tabIndex="5">
+    The Champ is here
+  </h1>
+);
+
+//React functional Component
+
+const HeadingComponent = () => (
+  <div className="container">
+  <h1 className="heading">
+    John cena is going to win his 17th world championship
+  </h1>
+  </div>
+);
+
+console.log(heading);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
