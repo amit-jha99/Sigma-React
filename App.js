d9f34732906bbbd872a265722f6fe7392368647b
amit-jha99 <amitjha121299@gmail.com>
2025-01-19 20:55:45 +0530

    Refactor App.js to render a single heading and add Parcel scripts to package.json

--- App.js
+++ App.js
@@ -1,33 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child 1" }, [
-    React.createElement(
-      "h1",
-      { id: "heading" },
-      "Escape the Matrix..."
-    ),
-    React.createElement(
-      "h2",
-      { id: "heading" },
-      "Hello I am a heading h2 inside nested elements"
-    ),
-  ]),
-  React.createElement("div", { id: "child 2" }, [
-    React.createElement(
-      "h1",
-      { id: "heading" },
-      "Hello I am a heading inside nested elements"
-    ),
-    React.createElement(
-      "h2",
-      { id: "heading" },
-      "Hello I am a heading h2 inside nested elements"
-    ),
-  ]),
-]);
-// const heading = React.createElement("h1", { id: "heading" }, "Hello, React!");
-console.log(parent);
+// React.createElement =>Object=>HTML Element ( when we render it on the DOM)
+
+const heading = React.createElement("h1", {id:"heading"}, "Tough times create strong men");
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(heading);
