acd90edf7fd0f4f624c8eb499351c9cb0fadf86c
amit-jha99 <amitjha121299@gmail.com>
2025-01-20 00:31:51 +0530

    Update App.js to render HeadingComponent instead of raw heading

--- App.js
+++ App.js
@@ -22,4 +22,5 @@ const HeadingComponent = () => (
 
 console.log(heading);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+// root.render(heading);
+root.render(<HeadingComponent/>)
