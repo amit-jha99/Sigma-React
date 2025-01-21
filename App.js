3b6e09bc2804add16a3ddd91b68831dec4ba020b
amit-jha99 <amitjha121299@gmail.com>
2025-01-21 10:40:44 +0530

    Refactor App.js to enhance component structure and introduce Title component

--- App.js
+++ App.js
@@ -1,26 +1,45 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+/**
+ * 
+ * Header 
+ * - Logo
+ * - Navbar
+ * body
+ * - Search bar
+ * - Restaurant container
+ *    - Restaurant Card
+ * footer
+ *  -Copyright 
+ *  - Links
+ *  -Address
+ *  -Contact
+ * 
+ */
+
 // React.createElement =>Object=>HTML Element ( when we render it on the DOM)
 
 //JSX
-const heading = (
+const Title = () => (
   <h1 className="heading" tabIndex="5">
     The Champ is here
   </h1>
 );
-
 //React functional Component
-
+// Component Composition
 const HeadingComponent = () => (
   <div className="container">
-  <h1 className="heading">
-    John cena is going to win his 17th world championship
-  </h1>
+  <h2>{console.log("Hell Yeah")}</h2>
+    {/* <Title /> */}
+    {Title()}
+    <h1 className="heading">
+      John cena is going to win his 17th world championship
+    </h1>
   </div>
 );
 
-console.log(heading);
+// console.log();
 const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(heading);
-root.render(<HeadingComponent/>)
+root.render(<HeadingComponent />);
