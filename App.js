a7640f166ef0f80abb1206a10f161d644878e659
amit-jha99 <amitjha121299@gmail.com>
2025-01-21 10:51:16 +0530

    Refactor App.js to introduce AppLayout component and improve structure

--- App.js
+++ App.js
@@ -19,28 +19,19 @@ import ReactDOM from "react-dom/client";
  * 
  */
 
-// React.createElement =>Object=>HTML Element ( when we render it on the DOM)
 
-//JSX
-const Title = () => (
-  <h1 className="heading" tabIndex="5">
-    The Champ is here
-  </h1>
-);
-//React functional Component
-// Component Composition
-const HeadingComponent = () => (
-  <div className="container">
-  <h2>{console.log("Hell Yeah")}</h2>
-    {/* <Title /> */}
-    {Title()}
-    <h1 className="heading">
-      John cena is going to win his 17th world championship
-    </h1>
-  </div>
-);
+const AppLayout = ()=>{
+  return (
+    <div className="app">
+    //Header 
+    //Body 
+    //Footer
 
-// console.log();
+    </div>
+  )
+}
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(heading);
 root.render(<HeadingComponent />);
