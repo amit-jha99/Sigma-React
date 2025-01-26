ae1465a7ed71d6646726fee41de8f7f3f93e7811
amit-jha99 <amitjha121299@gmail.com>
2025-01-26 14:58:06 +0530

    Refactor App.js to add Body and RestaurantCard components, enhancing layout and structure

--- App.js
+++ App.js
@@ -3,59 +3,87 @@ import ReactDOM from "react-dom/client";
 
 /**
  * Planning of Food Delivery App
- * 
- * Header 
+ *
+ * Header
  * - Logo
  * - Navbar
  * body
  * - Search bar
  * - Restaurant container
  *    - Restaurant Card
+ *      -img
+ *      - Name of restaurant,star Rating,cuisines delivery time
  * footer
- *  -Copyright 
+ *  -Copyright
  *  - Links
  *  -Address
  *  -Contact
- * 
+ *
  */
 
-const Header  = ()=>{
+const Header = () => {
   return (
     <div className="header">
       <div className="logo-container">
-        <img className="logo" src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?nwm=1&nws=1&industry=fast-food&sf=&txt_keyword=All" />
+        <img
+          className="logo"
+          src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?nwm=1&nws=1&industry=fast-food&sf=&txt_keyword=All"
+        />
       </div>
 
-      <div className ="nav-items">
-          <ul>
-            <li>Home</li>
-            <li>About us</li>
-            <li>Contact us</li>
-            <li>Cart</li>
-          </ul>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About us</li>
+          <li>Contact us</li>
+          <li>Cart</li>
+        </ul>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-const Body = ()=>{
-  return(
+const styleCard = {
+  backgroundColor: "#f0f0f0",
+};
+
+const RestaurantCard = () => {
+  return (
+    <div className="res-card" style={styleCard}>
+      <img className="res-logo" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/o6prrzzimmmnsgeuxf73" alt="res-menu" />
+      <h3>Meghana Foods</h3>
+      <h4>Biryani, North Indian,Asian</h4>
+      <h4>4.5 star</h4>
+      <h4>30 minutes</h4>
+    </div>
+  );
+};
+const Body = () => {
+  return (
     <div className="body">
-
+      <div className="search">Search</div>
+      <div className="res-container">
+        {/* //Restaurantcard  a Seperate component */}
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-const AppLayout = ()=>{
+const AppLayout = () => {
   return (
     <div className="app">
-   <Header />
-
+      <Header />
+      <Body />
     </div>
-  )
-}
-
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(heading);
