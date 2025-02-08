65450578e8d39e4fb5674d89b994e9e6d46b4f6f
amit-jha99 <amitjha121299@gmail.com>
2025-02-08 10:40:53 +0530

    Initialize list of restaurants in Body component and implement filtering for top-rated restaurants

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,46 +4,46 @@ import { useState } from "react";
 
 const Body = () => {
     //local state variable ->super powerful variable
-    const[listOfRestaurants] = useState([])
-//   let listOfRestaurants = [
-//     {
-//       info: {
-//         id: "9866",
-//         name: "Pizza Hut",
-//         cloudinaryImageId:
-//           "RX_THUMBNAIL/IMAGES/VENDOR/2024/7/16/1625d09a-2bfa-49c0-8797-780c096367f1_9866.jpg",
-//         locality: "Thakur Village",
-//         areaName: "Kandivali East",
-//         costForTwo: "₹300 for two",
-//         cuisines: ["Pizzas"],
-//         avgRating: 4.3,
-//       },
-//     },
-//     {
-//       info: {
-//         id: "37968",
-//         name: "Burger King",
-//         cloudinaryImageId:
-//           "RX_THUMBNAIL/IMAGES/VENDOR/2024/6/11/55d0daa1-ad2e-4893-be06-4709c5c68d49_37968.JPG",
-//         locality: "Thakur Village",
-//         areaName: "Kandivali East",
-//         costForTwo: "₹300 for two",
-//         cuisines: ["Pizzas"],
-//         avgRating: 3.5,
-//       },
-//     },
-//   ];
+    const[listOfRestaurants,setlistOfRestaurants] = useState([ {
+        info: {
+          id: "9866",
+          name: "Pizza Hut",
+          cloudinaryImageId:
+            "RX_THUMBNAIL/IMAGES/VENDOR/2024/7/16/1625d09a-2bfa-49c0-8797-780c096367f1_9866.jpg",
+          locality: "Thakur Village",
+          areaName: "Kandivali East",
+          costForTwo: "₹300 for two",
+          cuisines: ["Pizzas"],
+          avgRating: 4.3,
+        },
+      }
+    ,{
+        info: {
+          id: "37968",
+          name: "Burger King",
+          cloudinaryImageId:
+            "RX_THUMBNAIL/IMAGES/VENDOR/2024/6/11/55d0daa1-ad2e-4893-be06-4709c5c68d49_37968.JPG",
+          locality: "Thakur Village",
+          areaName: "Kandivali East",
+          costForTwo: "₹300 for two",
+          cuisines: ["Pizzas"],
+          avgRating: 3.5,
+        },
+      }])
+  
   return (
     <div className="body">
       <div className="filter">
         <button
           className="filter-btn"
           onClick={() => {
-            //Filter restaurant here
-           listOfRestaurants = listOfRestaurants.filter(
+            
+            // Filter restaurant here
+           filteredList = listOfRestaurants.filter(
               (restaurant) => restaurant.info.avgRating > 4
             );
-            console.log(listOfRestaurants);
+            setlistOfRestaurants(filteredList)
+            // console.log(listOfRestaurants);
           }}
         >
           Top Rated Restaurants
