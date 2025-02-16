8018618f908a4fd93f11f6cb0a1aad0a79561025
amit-jha99 <amitjha121299@gmail.com>
2025-02-16 14:46:19 +0530

    Add Error component and integrate it into the app router for error handling

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Header from "./components/Header";
 import Body from "./components/Body";
 import About from "./components/About";
 import Contact from "./components/Contact";
+import Error from "./components/Error";
 
 
 
@@ -30,6 +31,7 @@ const appRouter = createBrowserRouter([
   {
     path: "/",
     element: <AppLayout />,
+    errorElement: <Error/>
   },
   {
     path: "/about",
