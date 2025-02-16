5f8733fd15dce7829ee0ed1a40110d65df22a56f
amit-jha99 <amitjha121299@gmail.com>
2025-02-16 15:15:16 +0530

    Add routing for About and Contact pages; update Header component with navigation links

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider} from "react-router-dom";
+import { createBrowserRouter, Outlet, RouterProvider} from "react-router-dom";
 
 import Header from "./components/Header";
 import Body from "./components/Body";
@@ -22,7 +22,7 @@ const AppLayout = () => {
   return (
     <div className="app">
       <Header />
-      <Body />
+      <Outlet />
     </div>
   );
 };
@@ -31,16 +31,23 @@ const appRouter = createBrowserRouter([
   {
     path: "/",
     element: <AppLayout />,
+    children: [
+      {
+        path : "/",
+        element: <Body/>
+      },
+      {
+        path: "/about",
+        element: <About/>,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      }
+    ],
     errorElement: <Error/>
   },
-  {
-    path: "/about",
-    element: <About/>,
-  },
-  {
-    path: "/contact",
-    element: <Contact />,
-  }
+  
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { LOGO_URL } from "../utils/constants";
+import { Link } from "react-router-dom";
 const Header = () => {
   // let btnName = "Login";
   const [btnName, setbtnName] = useState("Login");
@@ -20,9 +21,9 @@ const Header = () => {
 
       <div className="nav-items">
         <ul>
-          <li>Home</li>
-          <li>About us</li>
-          <li>Contact us</li>
+          <li><Link to ="/">Home</Link></li>
+          <li><Link to ="/about">About us</Link></li>
+          <li><Link to = "/contact">Contact us</Link></li>
           <li>Cart</li>
           <button
             className="login-btn"
