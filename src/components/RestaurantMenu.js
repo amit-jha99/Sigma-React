5abf9677aca73c0f5dff749c1bb3f681922d92c9
amit-jha99 <amitjha121299@gmail.com>
2025-02-18 13:19:20 +0530

    Fetch restaurant menu data using dynamic API endpoint; update rendering logic in RestaurantMenu component

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,48 +1,51 @@
 import { useState, useEffect } from "react";
 import Shimmer from "./Shimmer";
+import { useParams } from "react-router-dom";
+import { MENU_API } from "../utils/constants";
 const RestaurantMenu = () => {
   const [resInfo, setResInfo] = useState(null);
+  const { resId } = useParams();
 
   useEffect(() => {
     fetchMenu();
   }, []);
 
   const fetchMenu = async () => {
-    const data = await fetch(
-      "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=19.2107021&lng=72.8699379&restaurantId=9866&catalog_qa=undefined&submitAction=ENTER"
-    );
+    const data = await fetch(MENU_API + resId);
+
     const json = await data.json();
-    
-    console.log(json);
-  
+
     setResInfo(json.data);
   };
 
-  if(resInfo === null){
-    return <Shimmer/>
+  if (resInfo === null) {
+    return <Shimmer />;
   }
 
-  const{cuisines,name,costForTwoMessage} =resInfo?.cards[2]?.card?.card.info;
+  const { cuisines, name, costForTwoMessage } =
+    resInfo?.cards[2]?.card?.card.info;
 
-  const {itemCards} = resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card
+  const { itemCards } =
+    resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card;
   console.log(itemCards);
 
-  return  (
+  return (
     <div className="menu">
-       <h1>{name}</h1>
-      <h3>{cuisines.join(",")} - {costForTwoMessage}</h3> 
+      <h1>{name}</h1>
+      <h3>
+        {cuisines.join(",")} - {costForTwoMessage}
+      </h3>
       <h3></h3>
       <h2>Menu</h2>
       <ul>
-      {
-        itemCards.map((item)=>{
-          return <li key ={item.card.info.id}>
-          {item.card.info.name}-{" Rs. "}
-          {item.card.info.price/100 || item.card.info.defaultPrice/100}
-          </li>
-        })
-      }
-       
+        {itemCards.map((item) => {
+          return (
+            <li key={item.card.info.id}>
+              {item.card.info.name}-{" Rs. "}
+              {item.card.info.price / 100 || item.card.info.defaultPrice / 100}
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
