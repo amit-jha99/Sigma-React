5fdff7b12045e2c852199e8eaacb7a55d434cf77
amit-jha99 <amitjha121299@gmail.com>
2025-02-19 00:24:13 +0530

    Add User component to display user information

---
+++ src/components/User.js
@@ -0,0 +1,9 @@
+const User = () => {
+  return <div className="user-card">
+    <h2>Name : Amit Jha</h2>
+    <h3>Location:Mumbai(mayanagri)</h3>
+    <h4>contact :@AmitJha101</h4>
+  </div>;
+};
+
+export default User;
