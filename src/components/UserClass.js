03e4d28f4537d5f22697a44ca395f80da5dca7b4
amit-jha99 <amitjha121299@gmail.com>
2025-02-19 18:25:27 +0530

    Refactor UserClass component to simplify state management and enhance button functionality for count increment

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -1,21 +1,26 @@
 import React from "react";
 class UserClass extends React.Component {
-
-    constructor(props) {
-        super(props);
-        console.log(props);
-        this.state={
-            count:0,
-            count2 :2,
-        };
-    }
+  constructor(props) {
+    super(props);
+    console.log(props);
+    this.state = {
+      count: 0,
+      count2: 2,
+    };
+  }
   render() {
     const { name, location } = this.props;
-    const{count,count2} = this.state;
+    const { count } = this.state;
     return (
       <div className="user-card">
-       <h1>count1:{count}</h1>
-       <h1>count2:{count2}</h1>
+        <h1>count1:{count}</h1>
+        <button onClick={()=>{
+            //Never update state variable directly
+            this.setState({
+                count: this.state.count+1,
+            });
+        }}>countIncrease</button>
+
         <h2>Name : {name}</h2>
         <h3>Location:{location}</h3>
         <h4>contact :@AmitJha101</h4>
