dd24c58e91ee1cad9dcf6c7f0a726a45ff1e36bf
amit-jha99 <amitjha121299@gmail.com>
2025-02-19 16:56:31 +0530

    Refactor UserClass component to destructure props for cleaner code

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -6,10 +6,11 @@ class UserClass extends React.Component {
         console.log(props);
     }
   render() {
+    const { name, location } = this.props;
     return (
       <div className="user-card">
-        <h2>Name : {this.props.name}</h2>
-        <h3>Location:{this.props.location}</h3>
+        <h2>Name : {name}</h2>
+        <h3>Location:{location}</h3>
         <h4>contact :@AmitJha101</h4>
       </div>
     );
