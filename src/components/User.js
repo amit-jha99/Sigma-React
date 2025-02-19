f51cff040672a7313f2434c1411a2ddb08e2c752
amit-jha99 <amitjha121299@gmail.com>
2025-02-19 17:25:20 +0530

    Add state management to User and UserClass components for count display

--- src/components/User.js
+++ src/components/User.js
@@ -1,5 +1,8 @@
+import { useState } from "react";
 const User = ({name}) => {
+    const [count] = useState(0);
   return <div className="user-card">
+    <h1>count :{count}</h1>
     <h2>Name : {name}</h2>
     <h3>Location:Mumbai(mayanagri)</h3>
     <h4>contact :@AmitJha101</h4>
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -4,11 +4,15 @@ class UserClass extends React.Component {
     constructor(props) {
         super(props);
         console.log(props);
+        this.state={
+            count:0,
+        };
     }
   render() {
     const { name, location } = this.props;
     return (
       <div className="user-card">
+       <h1>count:{this.state.count}</h1>
         <h2>Name : {name}</h2>
         <h3>Location:{location}</h3>
         <h4>contact :@AmitJha101</h4>
