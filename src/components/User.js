85db5630d440d25a9b6e74d4d4abda3a542a9167
amit-jha99 <amitjha121299@gmail.com>
2025-02-19 18:03:11 +0530

    Add second count state to User and UserClass components for enhanced display

--- src/components/User.js
+++ src/components/User.js
@@ -1,8 +1,10 @@
 import { useState } from "react";
 const User = ({name}) => {
     const [count] = useState(0);
+    const [count2] = useState(1);
   return <div className="user-card">
-    <h1>count :{count}</h1>
+    <h1>count1 :{count}</h1>
+    <h1>count2:{count2}</h1>
     <h2>Name : {name}</h2>
     <h3>Location:Mumbai(mayanagri)</h3>
     <h4>contact :@AmitJha101</h4>
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -6,13 +6,16 @@ class UserClass extends React.Component {
         console.log(props);
         this.state={
             count:0,
+            count2 :2,
         };
     }
   render() {
     const { name, location } = this.props;
+    const{count,count2} = this.state;
     return (
       <div className="user-card">
-       <h1>count:{this.state.count}</h1>
+       <h1>count1:{count}</h1>
+       <h1>count2:{count2}</h1>
         <h2>Name : {name}</h2>
         <h3>Location:{location}</h3>
         <h4>contact :@AmitJha101</h4>
