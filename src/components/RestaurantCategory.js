4214680a7225d64f9f79ef767839be58fe699246
amit-jha99 <amitjha121299@gmail.com>
2025-07-13 01:54:05 +0530

    Fix styling of title in RestaurantCategory component header for better visibility

--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -3,7 +3,8 @@ const RestaurnantCategory = ({data})=>{
     return <div>
         {/* Accordian header */}
        <div className="w-6/12 mx-auto my-4 bg-gray-50 shadow-lg p-4 flex justify-between">
-        <span>{data.title}</span>
+        <span className="font-bold text-2xl">{data.title}</span>
+        <span>{data.itemCards.length}</span>
         <span>⬇️</span>
     
        </div>
