3f48f102c511e002462306f6fda18e016dbf7b8b
amit-jha99 <amitjha121299@gmail.com>
2025-07-13 01:42:52 +0530

    Refactor RestaurantCategory component to display category title and icon

--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -2,7 +2,11 @@ const RestaurnantCategory = ({data})=>{
     console.log(data);
     return <div>
         {/* Accordian header */}
-        <h2 className="font-bold text-xl my-4">I made it in city of dreams</h2>
+       <div>
+        <span>{data.title}</span>
+        <span>⬇️</span>
+    
+       </div>
         {/* Accordian body */}
     </div>
 }
