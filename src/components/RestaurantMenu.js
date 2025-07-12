337b5d8bdbb0540e6cb75f94429c7eb9cb8bff90
amit-jha99 <amitjha121299@gmail.com>
2025-07-13 00:57:20 +0530

    Refactor RestaurantMenu component for improved readability and structure

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -20,7 +20,12 @@ const RestaurantMenu = () => {
   console.log(resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards);
 
   const categories =
-    resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter( c => c.card?.card?.["@type"] === "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory");
+    resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter(
+      (c) =>
+        c.card?.card?.["@type"] ===
+        "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
+    );
+
   console.log(categories);
 
   return (
@@ -31,16 +36,17 @@ const RestaurantMenu = () => {
       </h3>
       <h3></h3>
       <h2>Menu</h2>
-      <ul>
-        {itemCards.map((item) => {
-          return (
-            <li key={item.card.info.id}>
-              {item.card.info.name}-{" Rs. "}
-              {item.card.info.price / 100 || item.card.info.defaultPrice / 100}
-            </li>
-          );
-        })}
-      </ul>
+      {itemCards?.length > 0 && (
+  <ul>
+    {itemCards.map((item) => (
+      <li key={item.card.info.id}>
+        {item.card.info.name} - Rs.{" "}
+        {item.card.info.price / 100 || item.card.info.defaultPrice / 100}
+      </li>
+    ))}
+  </ul>
+)}
+
     </div>
   );
 };
