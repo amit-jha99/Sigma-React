3254dc26a7924daded7d1898328775ea6b9d22f1
amit-jha99 <amitjha121299@gmail.com>
2025-07-13 01:36:47 +0530

    Fix typo in RestaurantCategory component and add data prop for rendering

--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -1,6 +1,9 @@
-const RestaurnantCategory = ()=>{
+const RestaurnantCategory = ({data})=>{
+    console.log(data);
     return <div>
-
+        {/* Accordian header */}
+        <h2 className="font-bold text-xl my-4">I made it in city of dreams</h2>
+        {/* Accordian body */}
     </div>
 }
 
